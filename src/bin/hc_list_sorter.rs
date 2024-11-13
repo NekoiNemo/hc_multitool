87abcae346284374baa472e6efb44dbeeae6631f
nekoinemo <nekoinemo@gmail.com>
2024-11-13 19:22:30 +0300

    add email deduplication

--- src/bin/hc_list_sorter.rs
+++ src/bin/hc_list_sorter.rs
@@ -5,10 +5,6 @@ use std::io::{BufReader, BufWriter};
 
 use serde_json::Value;
 
-const COSMETICS_LISTS: [&str; 5] = ["facelist", "hairlist", "jacketlist", "jewllist", "shirtlist"];
-const FURN_LIST: &str = "furnlist";
-const FURN_FIXED: [&str; 2] = ["computer1", "hc_journal"];
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -29,6 +25,10 @@ fn main() {
         .as_object_mut()
         .expect("key save_data_key isn't an object");
 
+    // ======== Cosmetics lists sorting
+
+    const COSMETICS_LISTS: [&str; 5] = ["facelist", "hairlist", "jacketlist", "jewllist", "shirtlist"];
+
     for name in COSMETICS_LISTS {
         let arr = save_data
             .get_mut(name)
@@ -42,6 +42,10 @@ fn main() {
                 .to_string()
         });
     }
+
+    // ======== Furniture sorting
+
+    const FURN_LIST: &str = "furnlist";
 
     let furn_list = save_data
         .get_mut(FURN_LIST)
@@ -62,7 +66,35 @@ fn main() {
         FurnLabel(name.to_string())
     });
 
-    // ==== Output
+    // ======== Email deduping
+
+    let mut email_ids: Vec<i64> = Vec::with_capacity(32);
+
+    let mut email_dedup = |json_key: &str| {
+        let emails = save_data
+            .get_mut(json_key)
+            .unwrap_or_else(|| panic!("failed to find key: {json_key}"))
+            .as_array_mut()
+            .unwrap_or_else(|| panic!("key {json_key} wasn't an array"));
+
+        // emails are stored in the same way they are shown in-game: newer first
+        for i in (0..emails.len()).rev() {
+            let id = emails[i]
+                .as_i64()
+                .unwrap_or_else(|| panic!("key {json_key} expected to be an array of integers"));
+
+            if email_ids.contains(&id) {
+                emails.remove(i);
+            } else {
+                email_ids.push(id);
+            }
+        }
+    };
+
+    email_dedup("emailreadlist");
+    email_dedup("emailunreadlist");
+
+    // ======== Output
 
     let out_tmp = format!("{input_path}.new");
     let out_file = File::create(&out_tmp).expect("failed to create out file");
@@ -81,6 +113,8 @@ impl PartialOrd for FurnLabel {
         Some(self.cmp(other))
     }
 }
+
+const FURN_FIXED: [&str; 2] = ["computer1", "hc_journal"];
 
 impl Ord for FurnLabel {
     fn cmp(&self, other: &Self) -> Ordering {
